28a0e1ae8e73b05d48d5ea4db00f77c43b922f20
hamed701326 <sadeghi0hamed@gmail.com>
2020-01-21 05:29:13 -0800

    -----

--- instagram/src/main/java/entities/Post.java
+++ instagram/src/main/java/entities/Post.java
@@ -1,15 +1,28 @@
 package entities;
 
+import configue.PersistenceEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Post {
+public class Post extends PersistenceEntity<Integer> {
+    private String caption;
+    private Float latitude;
+    private Float longitude;
+    private String imagePath;
+    private int imageSize;
+    private Date dateCreated;
+    private Date dateUpdated;
+    @ManyToOne()
+    private User user;
 
 
 }
