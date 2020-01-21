87c66653059879ffb7fdc6c3c7a28ec64a992a4b
hamed701326 <sadeghi0hamed@gmail.com>
2020-01-21 04:24:10 -0800

    create PersistenceEntity and Post

---
+++ instagram/src/main/java/entities/Post.java
@@ -0,0 +1,15 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Post {
+
+
+}
--- instagram/src/main/java/entities/User.java
+++ instagram/src/main/java/entities/User.java
@@ -1,7 +1,18 @@
 package entities;
 
+import configue.PersistenceEntity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+
+@Entity
 @Data
-public class User {
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends PersistenceEntity<Long> {
+    private String userName;
+    private String password;
+
 }
